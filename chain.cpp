9fd53c9e75a5459206cf33d2c152068256cf1fcf
Leo-Leonis <leo_leonis@YeLeandro.localdomain>
2022-04-13 16:46:12 +0200

    perfectioning

--- chain.cpp
+++ chain.cpp
@@ -1,31 +1,36 @@
 #include "chain.hpp"
 
+#include <algorithm>
+#include <cmath>
+
+// struct PPState -----------------------------------------------------
+// operator< , verifica se <p> è più vicino all'origine rispetto al punto <other>
+bool PPState::operator<(PPState const& other) const { return x < other.x; }
+
 // classe Hooke -------------------------------------------------------
-double Hooke::operator()(PPState const& p1, PPState const& p2) const
-{
-    double d = p1.x - p2.x;
-    if (d < 0) {d *= -1;};
-    /* double diff = d - m_l;
-    double force = (diff) * m_k;
-    return force; */
-    return (d - m_l) * m_k;
+
+// operator() , restituisce la forza elastica tra <p1> e <p2>
+double Hooke::operator()(PPState const& p1, PPState const& p2) const {
+  return (std::abs(p1.x - p2.x) - m_l) * m_k;
 }
 
-// classe Chain ------------------------------------------------------
-
+// classe Chain -------------------------------------------------------
+/* push_back() , inserisce un punto <pps> nel vettore <m_ppses> in coda
+e mette in ordine i vettori di <v>*/
 void Chain::push_back(PPState const& pps) {
-    m_ppses.push_back(pps);
+  m_ppses.push_back(pps);
+  std::sort(m_ppses.begin(), m_ppses.end());
 }
 
-int Chain::size() const {
-    return m_ppses.size();
-}
-    
-void Chain::evolve(double delta_t){
-    double acc = ;
+// size(), restituisce il numero di punti in <m_ppses>
+int Chain::size() const { return m_ppses.size(); }
+
+// evolve() , fa sviluppare il sistema di punti per un arco di tempo di <delta_t>
+void Chain::evolve(double delta_t) {
+  for (unsigned int i = 1; i < m_ppses.size() - 1; i++) {
+    double acc = 0;
+  }
 }
 
-std::vector<PPState> const& Chain::state() const {
-    return m_ppses;
-}
-
+// state()
+std::vector<PPState> const& Chain::state() const { return m_ppses; }
--- chain.hpp
+++ chain.hpp
@@ -5,38 +5,41 @@
 #include <vector>
 
 struct PPState {
-    double m{};
-    double x{};
-    double v{};
+  double m{};
+  double x{};
+  double v{};
+
+  bool operator<(PPState const& other) const;
 };
 
 class Hooke {
-    double m_k;
-    double m_l;
-public:
-    // costruttore
-    Hooke(double k = 0., double l = 0.) : m_k{k} , m_l{l}
-    {
-        assert(k >= 0. && l >= 0.); //valori sensati
-    };
+  double m_k;
+  double m_l;
 
-    double operator()(PPState const& p1, PPState const& p2) const;
+ public:
+  // costruttore
+  Hooke(double k = 0., double l = 0.) : m_k{k}, m_l{l} {
+    assert(k >= 0. && l >= 0.);  // valori sensati
+  };
+  // funzioni membro
+  double operator()(PPState const& p1, PPState const& p2) const;
 };
 
 class Chain {
-    Hooke m_inter;
-    std::vector<PPState> m_ppses;
+  Hooke m_inter;
+  std::vector<PPState> m_ppses;
 
-public:
-    Chain(Hooke const& inter) : m_inter{inter}
-    {
+ public:
+  // costruttore
+  Chain(Hooke const& inter): m_inter{inter}
+        {
 
-    };
-    
-    void push_back(PPState const& pps);
-    int size() const;
-    void evolve(double delta_t);
-    std::vector<PPState> const& state() const;
+        };
+  // funzioni membro
+  void push_back(PPState const& pps);
+  int size() const;
+  void evolve(double delta_t);
+  std::vector<PPState> const& state() const;
 };
 
-#endif 
+#endif
